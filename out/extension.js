1f60f53c434c833407a225a755d00379f530ae84
Federico Poggi <federicopoggi3@gmail.com>
2024-03-30 15:33:32 +0100

    Fixed Bug

--- out/extension.js
+++ out/extension.js
@@ -36,7 +36,7 @@ function activate(context) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('scafoldingcpp.helloWorld', () => {
+    let disposable = vscode.commands.registerCommand('scafoldingcpp.gettemplate', () => {
         vscode.window.showInputBox({ prompt: "Enter project name" }).then((projectName) => {
             if (projectName && vscode.workspace.workspaceFolders) {
                 const ROOT_FOLDER = vscode.workspace.workspaceFolders;
