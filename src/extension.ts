3c4840102136d80dab22bd08978a0a719d3bcbd6
Federico Poggi <federicopoggi3@gmail.com>
2024-03-30 15:34:37 +0100

    Vsce

--- src/extension.ts
+++ src/extension.ts
@@ -1,17 +1,13 @@
 import * as vscode from 'vscode';
 import * as fs from 'fs';
 import * as path from 'path';
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+
 export function activate(context: vscode.ExtensionContext) {
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
+
     console.log('Congratulations, your extension "scafoldingcpp" is now active!');
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with registerCommand
-    // The commandId parameter must match the command field in package.json
+
     let disposable = vscode.commands.registerCommand('scafoldingcpp.gettemplate', () => {
         vscode.window.showInputBox({ prompt: "Enter project name" }).then((projectName) => {
             if (projectName && vscode.workspace.workspaceFolders) {
